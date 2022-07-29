441e71d76182879e97b395c9a20bf9406c2ea7db
AhshanHabib26 <habib.net27@gmail.com>
2022-07-30 00:47:06 +0600

    Navbar Componnet Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './Componnets/Pages/Home/Home';
+import Navbar from './Componnets/Pages/Home/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/home' element={<Home/>}/>
