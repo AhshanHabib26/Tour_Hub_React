1c947350061af67447e56a471f57d02a61f03daf
AhshanHabib26 <habib.net27@gmail.com>
2022-07-30 00:21:15 +0600

    Initial Projects Setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        IMSThemes: {
+          primary: "#ff0066",
+          secondary: "#FFECEB",
+          accent: "#4A546C",
+          neutral: "#00152B",
+          "base-100": "#ffffff",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
